fe13c77a000a236169fc7fcb2a7dca9ae7a6b2d9
kevinsherrell <k.sherrell.1028@gmail.com>
2021-02-27 11:14:05 -0700

    user schema created

---
+++ model/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+const UserSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    zipCode: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    img: {
+        type: String
+    },
+    listings: [{
+        type: Schema.Types.ObjectId,
+        ref: "Listing"
+    }],
+
+})
+module.exports = mongoose.model('User', UserSchema);
