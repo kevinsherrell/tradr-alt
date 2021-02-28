d8a34f8dddbfc52dc194d7c34c77d2765ad66029
kevinsherrell <k.sherrell.1028@gmail.com>
2021-02-27 21:37:43 -0700

    get user by id working

--- controller/users.js
+++ controller/users.js
@@ -4,7 +4,7 @@ const userRouter = express.Router();
 const User = require('../model/User.js');
 
 // GET - get all users
-userRouter.get('/', (req, res) => {
+userRouter.get('/all', (req, res) => {
     console.log('user route working');
     User.find((err, users)=>{
         if(err){
@@ -14,6 +14,14 @@ userRouter.get('/', (req, res) => {
     });
 });
 // GET - get user by id
+userRouter.get('/:id', (req,res)=>{
+    User.findOne({_id: req.params.id}, (err, user)=>{
+        if(err){
+            res.status(500).send(err);
+        }
+        res.status(200).send(user);
+    })
+})
 // GET - get all users
 
 module.exports = userRouter;
