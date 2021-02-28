ef587b848f04c5514d74829632e51b5c85238f75
kevinsherrell <k.sherrell.1028@gmail.com>
2021-02-28 13:24:18 -0700

    seed data now working

--- controller/users.js
+++ controller/users.js
@@ -6,22 +6,88 @@ const User = require('../model/User.js');
 // GET - get all users
 userRouter.get('/all', (req, res) => {
     console.log('user route working');
-    User.find((err, users)=>{
-        if(err){
+    User.find((err, users) => {
+        if (err) {
             res.status(500).send(err);
         }
         res.status(200).send(users);
     });
 });
+// GET - seed user data
 
+userRouter.get('/seed',  (req, res) => {
+    console.log("Seed is working");
+    // const newUsers = [
+    //
+    // ]
+    User.insertMany([  {
+        firstName: 'Kevin',
+        lastName: "Sherrell",
+        zipCode: "62223",
+        email: "email@email.com",
+    }, {
+        firstName: 'Jony',
+        lastName: "Shantell",
+        zipCode: "84058",
+        email: "email2@email.com",
+    }, {
+        firstName: 'Tom',
+        lastName: "Ford",
+        zipCode: "62268",
+        email: "email3@email.com",
+    }, {
+        firstName: 'Steve',
+        lastName: "Mitchel",
+        zipCode: "63103",
+        email: "email4@email.com",
+    }, {
+        firstName: 'Virginia',
+        lastName: "Thompson",
+        zipCode: "22060",
+        email: "email5@email.com",
+    }, {
+        firstName: 'Kathy',
+        lastName: "Wexler",
+        zipCode: "75006",
+        email: "email6@email.com",
+    }, {
+        firstName: 'Janus',
+        lastName: "Masters",
+        zipCode: "75011",
+        email: "email7@email.com",
+    }, {
+        firstName: 'Kenneth',
+        lastName: "Michaels",
+        zipCode: "75015",
+        email: "email8@email.com",
+    }, {
+        firstName: 'Zenny',
+        lastName: "Adibe",
+        zipCode: "10001",
+        email: "email9@email.com",
+    }, {
+        firstName: 'Terrel',
+        lastName: "Sampson",
+        zipCode: "10003",
+        email: "email10@email.com",
+    }])
+        .then(users=>{
+            res.send(users)
+        }).catch(err=>{
+        res.send(err);
+    })
+
+});
 // GET - get user by id
-userRouter.get('/:id', (req,res)=>{
-    User.findOne({_id: req.params.id}, (err, user)=>{
-        if(err){
+userRouter.get('/:id', (req, res) => {
+    User.findOne({_id: req.params.id}, (err, user) => {
+        if (err) {
             res.status(500).send(err);
         }
         res.status(200).send(user);
     })
 })
+
+
 
 module.exports = userRouter;
--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
-const MONGO_URI = "mongodb://localhost:27017/tradr"
-const port = 3000;
+require('dotenv').config();
+const session = require('express-session');
+const MONGO_URI = process.env.MONGO_URI
+const port = process.env.PORT;
 
 // view engine
 app.set('view engine', 'ejs');
@@ -16,14 +18,18 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(methodOverride('_method'));
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
 
 // routes
 app.use('/auth', require('./controller/auth.js'));
 app.use('/user', require('./controller/users.js'));
-
-
+app.use('/seed', require('./controller/users.js'));
 // database connection
-mongoose.connect(MONGO_URI, {useNewUrlParser: true}, {useUnifiedTopology: true}, () => console.log("Connected to database"));
+mongoose.connect(MONGO_URI, {useNewUrlParser: true}, () => console.log("Connected to database"));
 
 // server listen
 app.listen(port, () => console.log(`server is listening on port: ${port}`));
