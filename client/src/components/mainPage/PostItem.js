98b33137adcf86be9d8db6a50611259898aba449
kevinsherrell <k.sherrell.1028@gmail.com>
2021-04-01 21:01:16 -0600

    files now get added to state

--- client/src/components/mainPage/PostItem.js
+++ client/src/components/mainPage/PostItem.js
@@ -12,7 +12,8 @@ class PostItem extends Component {
         category: "",
         itemsWanted: "",
         imageUrl: "",
-        description: ""
+        description: "",
+        images: []
     }
     handleCategory = (e) => {
         this.setState({
@@ -26,6 +27,11 @@ class PostItem extends Component {
             [e.target.name]: e.target.value
         }, () => console.log(this.state))
 
+    }
+    onFileChange = (e) => {
+        this.setState({
+            images: [...this.state.images, e.target.files[0]]
+        })
     }
     onSubmit = (e) => {
         e.preventDefault();
@@ -47,6 +53,7 @@ class PostItem extends Component {
 
     render() {
         console.log(this.props)
+        console.log(this.state)
         return (
             <>
 
@@ -59,10 +66,10 @@ class PostItem extends Component {
                     <div className="container">
                         <h2 className={"post-item__header"}>Ready to trade?</h2>
                         <p className={"post-item__sub-header"}>Enter your listing information below</p>
-                        <div className="post-item__form">
+                        <form className="post-item__form">
                             <div className="post-item__form-row-1">
                                 <div className="post-item__form-title">
-                                    <p className={"post-item__form-title-label"}>Headline</p>
+                                    <label className={"post-item__form-title-label"}>Headline</label>
                                     <input name={"title"} type="text" className="post-item__form-title-input"
                                            placeholder={"Headline"}
                                            value={this.state.title} onChange={this.onChange}/>
@@ -70,7 +77,7 @@ class PostItem extends Component {
                             </div>
                             <div className="post-item__form-row-2">
                                 <div className="post-item__form-category">
-                                    <p className={"post-item__form-category-label"}>Category</p>
+                                    <label className={"post-item__form-category-label"}>Category</label>
                                     <select className={'post-item__form-category-input'} onChange={this.handleCategory}
                                             value={this.state.category}>
                                         <option>Category</option>
@@ -85,22 +92,34 @@ class PostItem extends Component {
                                     </select>
                                 </div>
                                 <div className="post-item__form-image">
-                                    <p className={"post-item__form-image-label"}>Image</p>
-                                    <input name={"imageUrl"} type="text" className={'post-item__form-image-input'}
-                                           placeholder={"Image Url"} value={this.state.imageUrl}
-                                           onChange={this.onChange}/>
+                                    <label className={"post-item__form-image-label"}>Image</label>
+                                    {/*New - image upload*/}
+                                    <input name={"imageUrl"} type="file"
+                                           className={'post-item__form-image-input'}
+                                           placeholder={"Image Url"}
+                                           value={this.state.imageUrl}
+                                           onChange={this.onFileChange}
+                                    />
+
+
+
+
+                                    {/*Original - image url*/}
+                                    {/*<input name={"imageUrl"} type="text" className={'post-item__form-image-input'}*/}
+                                    {/*placeholder={"Image Url"} value={this.state.imageUrl}*/}
+                                    {/*onChange={this.onChange}/>*/}
                                 </div>
                             </div>
                             <div className="post-item__form-row-3">
                                 <div className="post-item__form-items-wanted">
-                                    <p className={"post-item__form-items-wanted-label"}>Items Wanted</p>
+                                    <label className={"post-item__form-items-wanted-label"}>Items Wanted</label>
                                     <input name={"itemsWanted"} type="text"
                                            className={'post-item__form-items-wanted-input'}
                                            placeholder={"Items Wanted"} value={this.state.itemsWanted}
                                            onChange={this.onChange}/>
                                 </div>
                                 <div className="post-item__form-price">
-                                    <p className={"post-item__form-price-label"}>Price With trade</p>
+                                    <label className={"post-item__form-price-label"}>Price With trade</label>
                                     <input name={"price"} type="number" className={"post-item__form-price-input"}
                                            placeholder={"Price with trade"} onChange={this.onChange}
                                            value={this.state.price}/>
@@ -108,16 +127,15 @@ class PostItem extends Component {
                             </div>
                             <div className="post-item__form-row-4">
                                 <div className="post-item__form-description">
-                                    <p className={"post-item__form-description-label"}>Description:</p>
+                                    <label className={"post-item__form-description-label"}>Description:</label>
                                     <textarea name="description" id="" cols="30" rows="10" resize={"none"}
                                               className={'post-item__form-description-input'}
                                               value={this.state.description} onChange={this.onChange}></textarea>
                                 </div>
 
                             </div>
+                        </form>
 
-
-                        </div>
                         <div className="post-item__form-submit" onClick={this.onSubmit}>
                             Submit
                         </div>
